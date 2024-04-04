8b9bdbb54fdcc1973365ca9772202ebb8e1e97cc
mrstrange47 <krayush25@gmail.com>
2024-04-04 23:21:03 +0530

    Build Rent and Read

--- src/main/java/com/example/crio/rentAndRead/RentAndReadApplication.java
+++ src/main/java/com/example/crio/rentAndRead/RentAndReadApplication.java
@@ -1,4 +1,4 @@
-package com.example.crio.RentAndRead;
+package com.example.crio.rentAndRead;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
