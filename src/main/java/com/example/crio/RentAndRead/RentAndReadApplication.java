cc9055741948a64bf079eea3e804ebf729beb6ea
mrstrange47 <krayush25@gmail.com>
2024-04-04 23:11:37 +0530

    Build Rent and Read

---
+++ src/main/java/com/example/crio/RentAndRead/RentAndReadApplication.java
@@ -0,0 +1,14 @@
+package com.example.crio.RentAndRead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentAndReadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentAndReadApplication.class, args);
+		System.out.println("Server Started Successfully");
+	}
+
+}
---
+++ src/test/java/com/example/crio/RentAndRead/RentAndReadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crio.RentAndRead;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentAndReadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
